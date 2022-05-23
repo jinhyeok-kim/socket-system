f55d041553f2943b91e2bcdaa508ea41bac49a5e
김진혁 <jinhyeok@gimjinhyeog-ui-MacBookPro.local>
2022-05-23 22:38:55 +0900

    ech system

--- Client.js
+++ Client.js
@@ -1,16 +1,33 @@
 const net = require('net');
+const readLine = require('readline');
 
-const client = net.createConnection({ port: 443, host: 'quakka-echosystem.herokuapp.com' }, () => {
-  // 'connect' listener.
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const client = net.createConnection({ port: 7777, host: 'localhost' }, () => {
   console.log('connected to server!');
-  client.write('Hello!!!!!\r\n');
+
+  rl.on('line', function(line){
+      if (line == 'exit') {
+        rl.close();
+      }
+      client.write(line);
+  });
+
+  rl.on('close', function(){
+      process.exit();
+  });
+
 });
 
 client.on('data', (data) => {
   console.log(data.toString());
-  // client.end();
+
 });
 
 client.on('end', () => {
   console.log('disconnected from server');
+  client.end();
 });
--- Server.js
+++ Server.js
@@ -5,16 +5,15 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const sendData = data.toString();
     console.log(sendData);
-    socket.write(sendData);
+    socket.write(`Server: ${sendData}`);
   });
-
-  // socket.end('goodbye server\n');
 }).on('error', (err) => {
   // Handle errors here.
   throw err;
 });
 
-server.listen(process.env.PORT , (socket) => {
+server.listen(7777 , (socket) => {
+// server.listen(process.env.PORT , (socket) => {
   console.log('opened server on', server.address());
   console.log(process.env.PORT);
 });
